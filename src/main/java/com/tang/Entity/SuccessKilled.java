73c2edd02c8bba0f2fb29345962467c97e0651e7
TSorzZZ <373685440@qq.com>
2022-08-22 19:13:22 +0800

    1.0 with SuccessKilled

---
+++ src/main/java/com/tang/Entity/SuccessKilled.java
@@ -0,0 +1,15 @@
+package com.tang.Entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class SuccessKilled {
+    private static final long serialVersionUID = 1834437127882846202L;
+
+    private long secKillId;
+    private long userPhone;
+    private short state;
+    private LocalDateTime createTime;
+    private Seckill seckill;
+
+}
